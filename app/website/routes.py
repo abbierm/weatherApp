ef5b92b2ad3924c304e12ea30d0267ab775fa5dd
Abbie <armiller1003@gmail.com>
2025-04-10 18:45:04 -0500

    added new CLI with typer, changed website styling, and updated website router functions to coorespond with new form.

--- app/website/routes.py
+++ app/website/routes.py
@@ -4,7 +4,6 @@ from app.website import website_router as r
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from ..dependencies import APIClient
-from typing import Optional
 from pydantic import BaseModel
 import jinja2
 
@@ -20,57 +19,46 @@ def index(request: Request):
 
 
 class LocationForm(BaseModel):
-    city: str
-    state: Optional[str]
-    country: Optional[str]
-    save_response: bool
+    location: str
+    parsed_location: str | None = None # For geocoordinates api
 
     @classmethod
-    def check_form(
-        cls, 
-        city: str = Form(...),
-        state: str | None = Form(""),
-        country: str | None = Form(""),
-        save_response: str | None = Form("")
+    def parse_location(
+        cls,
+        location: str = Form(...)
     ):
-        save_response_bool = True if save_response == "on" else False
-        city = city.replace(" ", "")
-        state = state.replace(" ", "")
-        country = country.replace(" ", "")
-        return cls(city=city, state=state, country=country,
-				                save_response=save_response_bool)
+        parsed_location = location.replace(", ", "+").replace(" ", "+")
+        return cls(location=location, parsed_location=parsed_location)
+        
 	
-
 @r.post("/get_weather", response_class=HTMLResponse)
 async def get_weather(
     request: Request,
-    form_data: LocationForm = Depends(LocationForm.check_form),
+    form_data: LocationForm = Depends(LocationForm.parse_location),
     client: APIClient = Depends(APIClient)
 ):
 	# config settings
     s = request.app.state.settings
-    
     # Geo coordinates
-    geo_form_string = f"{form_data.city}+{form_data.state}+{form_data.country}"
-    geo_url = s.base_geocoding_url + geo_form_string + "&api_key=" + s.geocoding_api_key
+    geo_url = s.base_geocoding_url + form_data.parsed_location + "&api_key=" + s.geocoding_api_key
     json_location = await client.query_url(url=geo_url)
 	
 	# Grab lat and lon
     try:
         lat, lon = json_location[0]['lat'], json_location[0]['lon']
         coordinates_string = f"lat={lat}&lon={lon}&appid={s.weather_api_key}"
-    except KeyError:
+    except KeyError or IndexError:
         # TODO: Turn this into flash message
-        return {"ERROR": f"{form_data.city} not found"}
+        return {"ERROR": f"{form_data.location} not found"}
 	
 	# Weather API
-    weather_url = f"{s.base_weather_url}&exclude=minutely&{coordinates_string}"
+    weather_url = f"{s.base_weather_url}&units=imperial&exclude=minutely&{coordinates_string}"
     json_weather = await client.query_url(url=weather_url)
     return templates.TemplateResponse(
 		"weather.html",
         {
             "request": request,
-            "location": form_data.city,
+            "location": form_data.location,
             "info": json_weather
         }
     )
--- weatherApp.py
+++ weatherApp.py
@@ -3,20 +3,24 @@ import uvicorn
 import typer
 from config import Settings
 
+cli = typer.Typer()
+
 
 class DevSettings(Settings):
     dev: bool = True
 
 app = create_app(DevSettings)
 
-def main(dev: bool = False):
-    if dev:
-        print("running dev mode")
-        uvicorn.run("weatherApp:app", host="0.0.0.0", port=8080, reload=True)
-    else:
-        weatherApp = create_app(Settings)
-        uvicorn.run(weatherApp, host="0.0.0.0", port=8080)
+@cli.command()
+def dev(reload: bool = True):
+    uvicorn.run("weatherApp:app", host="0.0.0.0", port=8080, reload=reload)
+
+
+@cli.command()
+def run(reload: bool = False):
+    weatherApp = create_app(Settings)
+    uvicorn.run(weatherApp, host="0.0.0.0", port=8080)
 
 
 if __name__ == "__main__":
-    main()
+    cli()
