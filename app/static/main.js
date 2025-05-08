ab28d478da8ba31c3dcb4091ff484d61e9ccc30d
abbie <sevenbplusdev@gmail.com>
2025-05-08 18:39:24 -0500

    added toggle functions to swith all values from metric to imperial

--- app/static/main.js
+++ app/static/main.js
@@ -10,29 +10,74 @@ function convertToFahrenheit(temp) {
     return Math.round((temp * 1.8) + 32);
 }
 
+function convertToPsi(pressure) {
+    var psiPressure = pressure * .029529980;
+    return psiPressure.toFixed(2);
+}
 
-function toggleTempUnits() {
-    var currentTemps = document.querySelectorAll(".temp");
-    var currentUnits = document.querySelectorAll(".units")
-    if (!celsius) {
-        currentTemps.forEach((el, index) => {
-            var newTemp = convertToCelsius(Number(el.innerHTML));
-            el.innerHTML = newTemp;
-        })
-        currentUnits.forEach((units, index) => {
-            units.innerHTML = '°C';
-        })
-        celsius = true;
-    }
-    else {
-        currentTemps.forEach((el, i) => {
-            var temp = convertToFahrenheit(Number(el.innerHTML));
-            el.innerHTML = temp;
-        })
-        currentUnits.forEach((un, i) => {
-            un.innerHTML ='°F';
-        })
-        celsius = false;
-    }
+function convertToMph(speed) {
+    return Math.round((speed * .621371));
+}
+
+
+
+function toggleToCelsius() {
     
+    if (!celsius) {
+        let currentTemps = document.querySelectorAll(".temp");
+        for (let el of currentTemps) {
+            let newTemp = convertToCelsius(Number(el.innerHTML));
+            el.innerHTML = newTemp;
+        }
+        
+        let currentUnits = document.querySelectorAll(".units");
+        for (let unit of currentUnits) {
+            unit.innerHTML = '°C';
+        }
+        celsius = true;
+
+        // 
+        let current = document.getElementById("large-celsius"), old = document.getElementById("large-fahrenheit");
+        current.classList.replace("inactive", "active");
+        old.classList.replace("active", "inactive");
+
+        // Pressure
+        let pressureElement = document.getElementById("pressure");
+        let p = pressureElement.dataset.name;
+        pressureElement.innerHTML = p + ' mbar';
+
+        // windspeed
+        let windSpeedElement = document.getElementById("wind-speed");
+        let s = Math.Round(windSpeedElement.dataset.name) + ' kph';
+        windSpeedElement.innerHTML = s;
+    }
+}
+
+function toggleToFahrenheit() {
+    
+    if (celsius) {
+        let currentTemps = document.querySelectorAll(".temp");
+        for (let el of currentTemps) {
+            let newTemp = convertToFahrenheit(Number(el.innerHTML));
+            el.innerHTML = newTemp;
+        }
+        let currentUnits = document.querySelectorAll(".units");
+        for (let unit of currentUnits) {
+            unit.innerHTML = '°F';
+        }
+        celsius = false;
+        let cur = document.getElementById("large-fahrenheit"), old = document.getElementById("large-celsius");
+        cur.classList.replace("inactive", "active");
+        old.classList.replace("active", "inactive");
+
+        // change the pressure
+        let pressureElement = document.getElementById("pressure");
+        let p = pressureElement.dataset.name;
+        pressureElement.innerHTML = convertToPsi(p) + ' psi';
+
+        // change windspeed
+        let windSpeedE = document.getElementById("wind-speed");
+        let s = windSpeedE.dataset.name;
+        windSpeedE.innerHTML = convertToMph(s) + ' mph';
+    }
 }
